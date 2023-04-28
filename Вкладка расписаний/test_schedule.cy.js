7e0cd7a914010cf2ef07dec605882c29f0e0b3ad
User@A0068 <User@A0068>
2023-04-28 16:11:28 +0300

    update schedule

--- Вкладка расписаний/test_schedule.cy.js
+++ Вкладка расписаний/test_schedule.cy.js
@@ -17,16 +17,16 @@ describe('Работа с вкладками расписаний', () => {
     
     // добавляем врача
     cy.contains("Добавить врача").click()
-    cy.get("[data-cy='practitioner-snils'] > div > .gdfUhX").type('01069948355')
+    cy.get("[data-cy='practitioner-snils'] > div > input").type('01069948355')
     cy.contains("ИСКАТЬ ВРАЧА", {timeout: 5000}).click()
     cy.contains("СОХРАНИТЬ", {timeout: 100000}).click()
 
     // проверяем фильтр ища созданного врача
     cy.get("[placeholder='Поиск']", {timeout: 100000}).type("Беляевский{enter}", {timeout: 100000})
-    cy.get(".sc-bSGrXo.fSXFoF").should("contain", "Беляевский Евгений Борисович")
+    cy.get("[data-cy='doctor-name-0']").should("contain", "Беляевский Евгений Борисович")
 
     //удаляем добавленное значение и проверяем что оно больше не находится
-    cy.get("div:nth-child(1) > div.sc-cwhxAJ.boWLaY > div > svg").click()
+    cy.get("[data-cy='remove-practitioner']", {timeout: 5000}).click()
     cy.contains("По запросу не нашлось результатов")
 
     // переходим ко вкладке "Профили"
@@ -38,48 +38,54 @@ describe('Работа с вкладками расписаний', () => {
     let profile_name = "auto_profile" + ms
 
     // вводим имя
-    cy.get("div:nth-child(3) > div > label > div:nth-child(2) > input").type(profile_name)
+    cy.get("[data-cy='profile-name'] > div > input").type(profile_name)
 
     // выбираем маршрут
-    cy.get("div.css-2b097c-container > div > div.css-1vdkdsd > div.css-229dyb-singleValue").click()
+    cy.get("[data-cy='profile-workflow'] > div > div").click()
     cy.get("[id$='option-0']").click()
 
     //атрибут возрастной категории граждан
-    cy.get("div:nth-child(2) > div:nth-child(2) > div > label > div > div > div.css-1vdkdsd").click()
+    cy.get("[data-cy='row-multi-select-1.2.643.2.69.1.1.1.223']").click()
     cy.get("[id$='option-0']").click()
 
     // атрибут номенклатуры мед услуг
-    cy.get("div:nth-child(3) > div:nth-child(2) > div > label > div > div > div.css-1vdkdsd", {timeout: 100000}).click()
+    cy.get("[data-cy='row-multi-select-1.2.643.5.1.13.13.11.1070']", {timeout: 100000}).click()
     cy.get("[id$='option-0']", {timeout: 100000}).click()
 
     // атрибут составного профиля помощи
-    cy.get("div:nth-child(4) > div:nth-child(2) > div > label > div > div > div.css-1vdkdsd").click()
+    cy.get("[data-cy='row-multi-select-1.2.643.2.69.1.1.1.56']").click()
     cy.get("[id$='option-0']").click()
 
     // атрибут типа предоставления услуги
-    cy.get("div:nth-child(5) > div:nth-child(2) > div > label > div > div > div.css-1vdkdsd").click()
+    cy.get("[data-cy='attribute-1.2.643.2.69.1.1.1.182.32']").click()
     cy.get("[id$='option-0']").click()
 
     cy.contains("СОХРАНИТЬ").click()
     cy.contains("Профиль " + profile_name + " сохранен")
 
     // проверям фильтр ища по названию созданный профиль
-    cy.get("div:nth-child(2) > input").type(profile_name)
+    cy.get("[data-cy='profile-filter-name'] > div > input").type(profile_name)
     cy.contains("ИСКАТЬ").click()
 
     // у найденного значения проверяем что название равно тому, что мы искали
-    cy.get("div.sc-cWvMYY.feVQYu > div").should("contain", profile_name)
+    cy.get("[data-cy='profile-name-0']").should("contain", profile_name)
+
+    // проверяем на созданном профиле блокировку
+    cy.get("[data-cy='profile-change-status-0']").click()
+    cy.contains("Заблокировано")
+    cy.get("[data-cy='profile-change-status-0']").click()
+    cy.contains("Активно")
 
     // переходим на вкладку расписаний и создаем новое расписание
-    cy.get("div.sc-qRumB.ehMjgb > div > div:nth-child(2) > div").click()
+    cy.get("#TMSchedule-container > div > div > div > div > [data-cy='menu-schedule']").click()
     cy.contains("ДОБАВИТЬ РАСПИСАНИЕ").click()
 
     // выбираем профиль
-    cy.get("div:nth-child(2) > label > div.css-2b097c-container > div > div.css-1vdkdsd").click()
+    cy.get("[data-cy='schedule-profile'] > div > div").click()
     cy.get("[id$='option-0']").click()
 
     // выбираем шаблон
-    cy.get("div:nth-child(3) > label > div.css-2b097c-container > div > div.css-1vdkdsd").click()
+    cy.get("[data-cy='schedule-template'] > div > div").click()
     cy.get("[id$='option-0']").click()
 
     let d = new Date()
@@ -90,11 +96,11 @@ describe('Работа с вкладками расписаний', () => {
     let date = year + "-" + month + "-" + day
 
     // вводим даты действия расписания
-    cy.get("[data-cy='schedule-start'] > div >.cINcOw").type(date)
-    cy.get("[data-cy='schedule-end'] > div >.cINcOw").type(date)
+    cy.get("[data-cy='schedule-start'] > div > input").type(date)
+    cy.get("[data-cy='schedule-end'] > div >input").type(date)
 
     // выбираем врача
-    cy.get("[data-cy='schedule-practitioners'] > div > div > div:nth-child(1)").click()
+    cy.get("[data-cy='schedule-practitioners'] > div > div").click()
     cy.get("[id$='option-0']").click()
 
     // сохраняем расписание
@@ -104,11 +110,11 @@ describe('Работа с вкладками расписаний', () => {
     // возвращаемся на страницу расписаний и фильтруем расписания по выбранному профилю
     cy.contains("Назад к списку").click()
 
-    cy.get("div.sc-ehsgIH.hRmpRK.sc-qRumB.fECQIy.FilterForm > div:nth-child(1) > label > div.css-2b097c-container > div > div.css-1vdkdsd").click()
+    cy.get("[data-cy='schedule-filter-profile'] > div > div").click()
     cy.get("[id$='option-0']").click()
 
     cy.get("[data-cy='schedule-search']").click()
-    cy.get("div.sc-bZPPFW.deEwFo > div.sc-cyRfQX.iUqHII > div").should("contain", "1_test_ui")
+    cy.get("[data-cy^='schedule-name']").should("contain", "1_test_ui")
 
     // выходим с портала
     cy.get(".menu-exit", {timeout: 5000}).click()
